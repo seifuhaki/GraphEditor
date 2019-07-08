53bf864bc93cbf374260f2923717728d4e20b5cc
seifuhaki <3170106091@zju.edu.cn>
2019-07-08 14:37:31 +0800

    Initial commit(UI)

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -23,13 +23,6 @@ MainWindow::MainWindow(QWidget *parent) :
     tabifyDockWidget(ui->dock_geometry, ui->dock_special);
 
     ui->dock_geometry->raise();             //指定窗口置于最前
-
-    ConnectFile();
-    //InitImage();        // 初始化图像QLabel
-    //InitLayerView();
-    ConnectAction();    // Initialize the action connection
-    //ConnectLayer();
-
     //皮肤！
     QFile styleSheet("./qss/main.qss");
     if (!styleSheet.open(QIODevice::ReadOnly))
@@ -43,34 +36,4 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
-}
-void MainWindow::ConnectFile()
-{
-    connect(ui->action_new, SIGNAL(triggered()), this, SLOT(NewFile()));
-    connect(ui->action_open, SIGNAL(triggered()), this, SLOT(OpenFile()));
-    connect(ui->action_save, SIGNAL(triggered()), this, SLOT(SaveFile()));
-    connect(ui->action_exit, SIGNAL(triggered()), this, SLOT(close()));
-    connect(ui->action_saveas, SIGNAL(triggered()), this, SLOT(SaveasFile()));
-}
-void MainWindow::ConnectAction(){
-    connect(ui->pushButton_choose,SIGNAL(clicked()),this,SLOT(SetActionDrag()));
-    //connect(imgLabel,SIGNAL(dragged(QPoint,QPoint)),this,SLOT(DragSlot(QPoint,QPoint)));
-    //connect(imgLabel,SIGNAL(moved(QPoint,QPoint)),this,SLOT(MoveSlot(QPoint,QPoint)));
-    connect(ui->pushButton_line,SIGNAL(clicked()),this,SLOT(Lines()));
-    connect(ui->pushButton_ciecle,SIGNAL(clicked()),this,SLOT(Circles()));
-    connect(ui->pushButton_rectangle,SIGNAL(clicked()),this,SLOT(Rect()));
-    connect(ui->pushButton_cut,SIGNAL(clicked()),this,SLOT(Tailor()));
-    connect(ui->pushButton_eraser,SIGNAL(clicked()),this,SLOT(Erase()));
-    connect(ui->confirm_filter_2,SIGNAL(clicked()),this,SLOT(Blur()));
-    connect(ui->pushButton_histogram,SIGNAL(clicked()),this,SLOT(Hist()));
-    connect(ui->clockwise_90,SIGNAL(clicked()),this,SLOT(Rotate90()));
-    connect(ui->anticlockwise_90,SIGNAL(clicked()),this,SLOT(AntiRotate90()));
-    connect(ui->confirm_rotate,SIGNAL(clicked()),this,SLOT(Rotate()));
-    connect(ui->confrim_size,SIGNAL(clicked()),this,SLOT(Resize()));
-    connect(ui->pushButton_color,SIGNAL(clicked()),this,SLOT(CallColorDialog()));
-    connect(ui->pushButton_translation,SIGNAL(clicked()),this,SLOT(Translation()));
-    connect(ui->pushButton_turn_h,SIGNAL(clicked()),this,SLOT(TrunH()));
-    connect(ui->pushButton_turn_v,SIGNAL(clicked()),this,SLOT(TrunV()));
-    connect(ui->confirm_filter,SIGNAL(clicked()),this,SLOT(Filter()));
-    connect(ui->pushButton_pen,SIGNAL(clicked()),this,SLOT(UsePainter()));
 }
